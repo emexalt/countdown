cf70c03bddacaa3f8b1adc5095069bb82e860465
Em Folsom <emmeryn.dev@gmail.com>
2020-09-09 15:44:17 -0500

    Adding code to validate word input against available letters

--- squares.js
+++ squares.js
@@ -15,11 +15,9 @@ function setCounts() {
     console.log("Requested vowels: " + vowelCount);
     for (let index = 1; index < consCount + 1; index++) {
         puzzleArray.push(consonants[Math.floor(Math.random() * consonants.length)]);
-        //document.getElementById("square" + index).innerText = consonants[Math.floor(Math.random() * consonants.length)];
     }
     for (let index = consCount + 1; index < 10; index++) {
         puzzleArray.push(vowels[Math.floor(Math.random() * vowels.length)]);
-        
     }
     puzzleArray.sort(function(a, b){return 0.5 - Math.random()});
     drawBoard();
@@ -31,3 +29,16 @@ function drawBoard() {
     }
 }
 
+//in progress; need to integrate check against word-list
+function checkGuess(){
+    guess = document.getElementById("guess").value.split("");
+    console.log(guess);
+    for (let index = 0; index <= guess.length-1; index++){
+        if (puzzleArray.includes(guess[index])) {
+            console.log("Found " + guess[index]);
+        } else {
+            console.log("Did not find " + guess[index]);
+         }
+    }
+
+}
