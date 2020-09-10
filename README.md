2ccf588849d392ffcfdbb3d2dbf1415a4b6d141d
Em Folsom <emmeryn.dev@gmail.com>
2020-09-09 23:56:12 -0500

    Now has answer validation, bring your own wordlist

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Countdown letters game visual
+# Countdown letters game
 
-This is (currently) a visual display to aid in playing the "letters game" from the Channel 4 television series *Countdown* on a video conferencing call or online video stream. Given a number of vowels, it returns a row of nine letter tiles, from which words can be made.
+This is a very simple "letters game" from the Channel 4 television series *Countdown* on a video conferencing call or online video stream. Given a number of vowels, it returns a row of nine letter tiles, from which words can be made.
 
-In this version:
+In this commit:
++ Answer validation! (word list not included, I use [Dwyl's English Words], specifically the alphanumeric-only version.(https://github.com/dwyl/english-words)
++ A pleasing circular indicator of whether or not one is correct!
 + Any number of vowels <= 9 can be requested before generating the puzzle.
 + The letters in the puzzle will display in a shuffled order, as if chosen one at a time as is done in the television program.
 + The availability of letters follows the Scrabble distribution, rather than the published *Countdown* distribution found [here](http://www.thecountdownpage.com/letters.htm); expect this to change soon.
--- squares.js
+++ squares.js
@@ -6,8 +6,19 @@ em folsom, 2020
 consonantString = 'bbccddddffggghhjkllllmmnnnnnnppqrrrrrrsssstttttvvwwxyyz'.toUpperCase();
 consonants = consonantString.split('');
 vowelString = "AAAAAAAAAEEEEEEEEEEEEIIIIIIIIIOOOOOOOOUUUU";
+dictionaryString = "";
 vowels = vowelString.split('');
 puzzleArray = [];
+/*
+if you're going to run this, replace words_alpha.txt with your preferred
+list of newline-separated words, ideally a smaller one!
+*/
+dictionaryFile = fetch("./words_alpha.txt")
+    .then(response => response.text())
+    .then(text => dictionaryString = text);
+
+
+
 
 function setCounts() {
     let vowelCount = parseInt(document.getElementById("vowel").value);
@@ -31,14 +42,32 @@ function drawBoard() {
 
 //in progress; need to integrate check against word-list
 function checkGuess(){
-    guess = document.getElementById("guess").value.split("");
-    console.log(guess);
-    for (let index = 0; index <= guess.length-1; index++){
-        if (puzzleArray.includes(guess[index])) {
-            console.log("Found " + guess[index]);
+    guess = document.getElementById("guess").value.toUpperCase();
+    guessArray = guess.split("");
+    rightAnswerCounter = 0;
+    for (let index = 0; index <= guessArray.length-1; index++){
+        if (puzzleArray.includes(guessArray[index])) {
+            rightAnswerCounter++;
+            if (rightAnswerCounter === guessArray.length){
+                console.log("It's all on the board!");
+            }
         } else {
-            console.log("Did not find " + guess[index]);
-         }
-    }
+            console.log("Did not find " + guessArray[index]);
+            document.getElementById("answerIndicator").style.backgroundColor = "red";
 
+         }
+    } 
+    checkDictionary();
+}
+
+function checkDictionary() {
+    guess = document.getElementById("guess").value;
+    if (dictionaryString.includes(guess)) {
+        console.log("It's a word!");
+        document.getElementById("answerIndicator").style.backgroundColor = "green";
+    } else {
+        console.log("It's not in the dictionary, sorry.");
+        document.getElementById("answerIndicator").style.backgroundColor = "red";
+
+    }
 }
